4b7ccc2de4d67edb10698ac07ffbd4fc64e1f0fc
haiyang <haiyang@weili.com>
2023-03-15 15:51:26 +0800

    add solution3/4

--- lt3/Code3.java
+++ lt3/Code3.java
@@ -1,9 +1,9 @@
 package lt3;
 
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度
@@ -11,11 +11,17 @@ import java.util.List;
 public class Code3 {
     public static void main(String[] args) {
         String s = "pwwkew";
-        int i = solution1(s);
-        System.out.println(i);
+        // int i = solution1(s);
+        // System.out.println(i);
+        //
+        // int i1 = solution2(s);
+        // System.out.println(i1);
 
-        int i1 = solution2(s);
-        System.out.println(i1);
+        // int i2 = solution3(s);
+        // System.out.println(i2);
+
+        int i3 = solution4WithQueue(s);
+        System.out.println(i3);
     }
 
     private static int solution1(String s){
@@ -43,6 +49,12 @@ public class Code3 {
         return strList.get(strList.size()-1).length();
     }
 
+    /**
+     * 	执行耗时:234 ms,击败了5.01% 的Java用户
+     * 	内存消耗:42.4 MB,击败了5.09% 的Java用户
+     * @param s
+     * @return
+     */
     private static int solution2(String s){
         char[] chars = s.toCharArray();
         String res = "";
@@ -67,7 +79,57 @@ public class Code3 {
         return res.length();
     }
 
+    /**
+     * 滑动窗口
+     * 	执行耗时:6 ms,击败了42.21% 的Java用户
+     * 	内存消耗:41.3 MB,击败了86.95% 的Java用户
+     * @param s
+     * @return
+     */
     private static int  solution3(String s){
-        return 0;
+        if(s.length()<2){
+            return s.length();
+        }
+        HashSet<Character> container = new HashSet<>();
+        int left = -1 , max = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if(!container.isEmpty()){
+                container.remove(s.charAt(right-1));
+            }
+            while (left + 1 < s.length() && !container.contains(s.charAt(left +1))){
+                container.add(s.charAt(left+1));
+                left++;
+            }
+            max = Math.max(max, container.size());
+        }
+
+        return max;
     }
+
+    /**
+     * 	执行耗时:27 ms,击败了14.87% 的Java用户
+     * 	内存消耗:41.5 MB,击败了63.14% 的Java用户
+     * @param s
+     * @return
+     */
+    private static int  solution4WithQueue(String s){
+        if(s.length()<2){
+            return s.length();
+        }
+        Queue<Character> queue = new LinkedBlockingQueue<>(s.length());
+        int left = -1 , max = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if(!queue.isEmpty()){
+                queue.poll();
+            }
+            while (left + 1 < s.length() && !queue.contains(s.charAt(left +1))){
+                queue.offer(s.charAt(left+1));
+                left++;
+            }
+            max = Math.max(max, queue.size());
+        }
+
+        return max;
+    }
+
 }
